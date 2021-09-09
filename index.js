0b491c53e1e9e3e958a665a65a0e28d7097ada96
CarlosAMoran <Carlosamoran18@gmail.com>
2021-09-09 11:57:28 -0400

    lab complete

--- index.js
+++ index.js
@@ -1,72 +1,72 @@
 const exampleMovies = require("./movies");
 
 function getAllMovieTitles(moviesObj) {
-  if (moviesObj.length == 0){
-    throw "Error: No movie titles given!"
-  };
+  if (moviesObj.length == 0) {
+    throw "Error: No movie titles given!";
+  }
   return moviesObj.map((key) => {
     return key.title;
   });
-};
+}
 
 function checkIfAnyMovieHasRating(moviesObj, rating) {
-  if (moviesObj.length == 0){
-    throw "Error: No movie titles given!"
-  };
-  if(!rating){
+  if (moviesObj.length == 0) {
+    throw "Error: No movie titles given!";
+  }
+  if (!rating) {
     rating = "G";
-  };
+  }
   return moviesObj.some((key) => {
     return key.rated == rating;
   });
-};
+}
 
 function findById(moviesObj, id) {
-  if (moviesObj.length == 0){
-    throw "Error: No movie titles given!"
-  };
+  if (moviesObj.length == 0) {
+    throw "Error: No movie titles given!";
+  }
   result = moviesObj.find((key) => {
     if (key.imdbID == id) {
       return { key };
     }
   });
-  if (!result){
+  if (!result) {
     result = null;
-  };
+  }
   return result;
-};
-// accidentally put return null  twice 
+}
+// accidentally put return null  twice
 
 function filterByGenre(moviesObj, genre) {
-  if (moviesObj.length == 0){
-    throw "Error: No movie titles given!"
-  };
+  if (moviesObj.length == 0) {
+    throw "Error: No movie titles given!";
+  }
   return moviesObj.filter((key) => {
-    return key.genre.toLowerCase().includes(genre.toLowerCase())
+    return key.genre.toLowerCase().includes(genre.toLowerCase());
   });
-};
+}
 
 function getAllMoviesReleasedAtOrBeforeYear(moviesObj, year) {
-  if (moviesObj.length == 0){
-    throw "Error: No movie titles given!"
-  };
-  return moviesObj.filter(({released}) => {
+  if (moviesObj.length == 0) {
+    throw "Error: No movie titles given!";
+  }
+  return moviesObj.filter(({ released }) => {
     return Number(released.slice(-4)) <= year;
   });
-};
+}
 // forgot to replace key with released
 
 function getRottenTomatoesScoreByMovie(moviesObj) {
-  if (moviesObj.length == 0){
-    throw "Error: No movie titles given!"
-  };
+  if (moviesObj.length == 0) {
+    throw "Error: No movie titles given!";
+  }
   return moviesObj.map((key) => {
     let score = key["ratings"].find((key2) => {
-      return key2.source == "Rotten Tomatoes"
+      return key2.source == "Rotten Tomatoes";
     });
-    return { [key.title]: score.value};
+    return { [key.title]: score.value };
   });
-};
+}
 
 // Do not change anything below this line.
 module.exports = {
